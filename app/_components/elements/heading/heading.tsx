a5e43b58232a3ea0bc329514b7b2723ad676a88e
Mogy <64847336+Mogy1945@users.noreply.github.com>
2026-01-31 02:51:25 -0800

    Update heading.tsx

--- app/_components/elements/heading/heading.tsx
+++ app/_components/elements/heading/heading.tsx
@@ -7,13 +7,13 @@ type Props = ComponentProps<"h1"> & {
   as: HeadingComponent;
 };
 
-const variantStyle = {
-    h1: "text-3xl md:text-4xl font-bold leading-tight",
+const variantStyle: Record<HeadingComponent, string> = {
+  h1: "text-3xl md:text-4xl font-bold leading-tight",
   h2: "text-2xl md:text-3xl font-bold leading-tight",
   h3: "text-xl md:text-2xl font-semibold leading-snug",
   h4: "text-lg md:text-xl font-semibold leading-snug",
   h5: "text-base md:text-lg font-medium leading-normal",
-
+};
 
 export const Heading = ({ as, children }: Props) => {
   const Component = as;
